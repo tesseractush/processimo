c222163b54750b66a68896c2957bff6249bd8def
Mrityunjay Srivastava <tesseractush@gmail.com>
2025-03-07 17:09:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
-  distDir: "dist",
+  distDir: "out",
   images: {
     unoptimized: true, },
   sassOptions: {
